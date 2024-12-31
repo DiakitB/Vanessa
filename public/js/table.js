9476c90656fcf07be5812810bca62caa6ac798e3
DiakitB <babacar.diakite01@gmail.com>
2024-12-31 16:42:01 -0500

    add more functionality to the file

--- public/js/table.js
+++ public/js/table.js
@@ -1,3 +1,10 @@
-
+$(document).ready(function() {
+    $('#financeTable').DataTable({
+      "paging": true,
+      "searching": true,
+      "ordering": true,
+      "info": true
+    });
+  });
 
  
