7f78b6dcb85921988b29b5efd8e749be6fef8398
DiakitB <babacar.diakite01@gmail.com>
2025-01-08 20:07:43 -0500

    removing the config file

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -122,3 +122,5 @@ exports.createGroceryList = asyncHandler(async (req, res) => {
 
   res.render("groceryListView", { totalRecipes: recipes.length, groceryList: groceryList });
 });
+
+console.log(convertToGrams(1, 'cup')); // 236.588
